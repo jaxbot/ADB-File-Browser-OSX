4384f005d744ad19eb021cf53fb45cffef2fbb75
Jonathan Warner <jaxbot@gmail.com>
2015-02-22 21:04:01 -0500

    Initial Commit

---
+++ ADB File Browser OSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ADB File Browser OSX
+//
+//  Created by Jonathan Warner on 2/22/15.
+//  Copyright (c) 2015 Jonathan Warner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
